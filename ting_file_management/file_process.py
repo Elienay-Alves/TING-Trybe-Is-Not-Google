da0fbd1b1cb471d4f443567ce308f31c4ff71c00
Elienay Alves <elienay8486@gmail.com>
2023-05-18 17:39:37 -0300

    feat(file_process.py): finished requirement 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if path_file not in [item["nome_do_arquivo"] for item in instance.items]:
+        file = txt_importer(path_file)
+        data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(file),
+            "linhas_do_arquivo": file,
+        }
+
+        instance.enqueue(data)
+        print(data, file=sys.stdout)
 
 
 def remove(instance):
