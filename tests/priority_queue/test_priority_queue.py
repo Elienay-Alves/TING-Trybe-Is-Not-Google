d93d3f88b328ceaf13a4b2bf3dc71d5311ca6f41
Elienay Alves <elienay8486@gmail.com>
2023-05-22 13:46:03 -0300

    feat(test_priority_queue.py): finish requirement 6

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,36 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
+
+data = [
+    {
+        "nome_do_arquivo": "name.txt",
+        "qtd_linhas": 1,
+        "linhas_do_arquivo": ["Lorem ipsum"],
+    },
+    {
+        "nome_do_arquivo": "name.txt",
+        "qtd_linhas": 23,
+        "linhas_do_arquivo": ["Lorem ipsum"],
+    },
+]
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    queue = PriorityQueue()
+
+    queue.enqueue(data[0])
+    assert len(queue.high_priority) == 1
+    assert len(queue.regular_priority) == 0
+
+    queue.enqueue(data[1])
+    assert len(queue.regular_priority) == 1
+    assert len(queue.high_priority) == 1
+
+    assert queue.search(1) == data[1]
+
+    queue.dequeue()
+    assert queue.dequeue() is not None
+    assert len(queue.high_priority) == 0
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        queue.search(3)
