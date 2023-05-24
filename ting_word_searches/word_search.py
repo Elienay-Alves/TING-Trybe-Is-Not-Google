ebb8a6d49eff7992e947991c132f94ecc4ad4958
Elienay Alves <elienay8486@gmail.com>
2023-05-24 00:41:53 -0300

    feat(exists_word.py): finish requirement 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = list()
+
+    for data in instance.items:
+        lines = "linhas_do_arquivo"
+        file_name = "nome_do_arquivo"
+        result.append(
+            {
+                "arquivo": data[file_name],
+                "ocorrencias": [
+                    {"linha": index + 1}
+                    for index, line in enumerate(data[lines])
+                    if word.lower() in line.lower()
+                ],
+                "palavra": word.lower(),
+            }
+        )
+    if not result[0]["ocorrencias"]:
+        return []
+    return result
 
 
 def search_by_word(word, instance):
